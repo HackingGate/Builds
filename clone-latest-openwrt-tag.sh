41a6beb099c088880c7c636c2ce52554b66d8325
HackingGate <i@hackinggate.com>
2021-02-07 13:43:53 +0900

    Fix

--- clone-latest-openwrt-tag.sh
+++ clone-latest-openwrt-tag.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a simple command exits with a non-zero status
 set -e
 
-VERSION=`./git-describe-latest-openwrt-tag.awk`
+VERSION=`./git-describe-latest-tag.awk https://github.com/openwrt/openwrt`
 echo "Cloning OpenWrt $VERSION"
 git clone --branch $VERSION https://github.com/openwrt/openwrt.git
 cd openwrt
