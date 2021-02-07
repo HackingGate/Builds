70398e2d116cd80d9733b70b742f40e012f74da5
HackingGate <i@hackinggate.com>
2021-02-07 13:50:56 +0900

    Add regex

--- clone-latest-openwrt-tag.sh
+++ clone-latest-openwrt-tag.sh
@@ -3,9 +3,9 @@
 # Exit immediately if a simple command exits with a non-zero status
 set -e
 
-VERSION=`./git-describe-latest-tag.awk https://github.com/openwrt/openwrt`
-echo "Cloning OpenWrt $VERSION"
-git clone --branch $VERSION https://github.com/openwrt/openwrt.git
+TAG=`./git-describe-latest-tag.awk https://github.com/openwrt/openwrt`
+echo "Cloning OpenWrt $TAG"
+git clone --branch $TAG https://github.com/openwrt/openwrt.git
 cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
