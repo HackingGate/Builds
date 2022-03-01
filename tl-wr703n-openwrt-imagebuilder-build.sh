9715cd8113341cf07986ab78761dab70883b1863
HackingGate <i@hackinggate.com>
2022-03-01 09:02:27 +0900

    Add SUBTARGET

--- tl-wr703n-openwrt-imagebuilder-build.sh
+++ tl-wr703n-openwrt-imagebuilder-build.sh
@@ -5,23 +5,25 @@ set -e
 
 OPENWRT_MAJOR_VERSION=`echo ${OPENWRT_VERSION} | grep -E -o '[0-9]+\.[0-9]+'`
 TARGET='ath79'
+SUBTARGET='tiny'
 DEVICE_NAME='tplink_tl-wr703n'
 
 # Workaround to fix ath79 missing wifi
 if [[ $OPENWRT_MAJOR_VERSION < '20.02' ]]; then
     TARGET='ar71xx'
+    SUBTARGET='generic'
     DEVICE_NAME='tl-wr703n-v1'
 fi
 
 echo "Download OpenWrt Image Builder ${OPENWRT_VERSION}"
 
 # Download imagebuilder for TL-WR703N.
-aria2c -c -x4 -s4 https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${TARGET}/tiny/openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64.tar.xz
+aria2c -c -x4 -s4 https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${TARGET}/${SUBTARGET}/openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-${SUBTARGET}.Linux-x86_64.tar.xz
 
 # Extract & remove used file & cd to the directory
-tar -xvf openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64.tar.xz
-rm openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64.tar.xz
-cd openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64/
+tar -xvf openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-${SUBTARGET}.Linux-x86_64.tar.xz
+rm openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-${SUBTARGET}.Linux-x86_64.tar.xz
+cd openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-${SUBTARGET}.Linux-x86_64/
 
 # Replace tiny-tp-link.mk
 cd target/linux/${TARGET}/image
