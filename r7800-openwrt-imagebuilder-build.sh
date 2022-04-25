2d968c52644b88ce883bd77d88dc8eda96f1ef68
HackingGate <8541644+HackingGate@users.noreply.github.com>
2022-04-25 22:52:17 +0900

    Remove adblock and add 6in4

--- r7800-openwrt-imagebuilder-build.sh
+++ r7800-openwrt-imagebuilder-build.sh
@@ -26,7 +26,7 @@ https-dns-proxy luci-app-https-dns-proxy \
 -wpad-mini -wpad-basic -wpad-basic-wolfssl wpad-wolfssl usbutils block-mount e2fsprogs samba4-server luci-app-samba4 \
 aria2 luci-app-aria2 ariang curl wget kmod-fs-ext4 kmod-usb-storage kmod-usb-storage-uas \
 luci-app-statistics collectd-mod-cpu collectd-mod-interface collectd-mod-memory collectd-mod-ping collectd-mod-rrdtool collectd-mod-wireless \
--dnsmasq dnsmasq-full luci-app-wireguard luci-proto-wireguard adblock luci-app-adblock tcpdump-mini \
+-dnsmasq dnsmasq-full luci-app-wireguard luci-proto-wireguard 6in4 \
 ddns-scripts ddns-scripts-cloudflare luci-app-ddns bind-host \
 diffutils git"
 
