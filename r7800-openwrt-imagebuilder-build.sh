a7c04314d253c206ef78186b9e91168f1dcb4c1e
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-10-15 17:48:49 +0900

    Use wpad-mbedtls

--- r7800-openwrt-imagebuilder-build.sh
+++ r7800-openwrt-imagebuilder-build.sh
@@ -21,9 +21,9 @@ sed -i -e "s/=m/=y/g" build_dir/target-arm_cortex-a15+neon-vfpv4_musl_eabi/linux
 
 # Run the final build configuration
 make image PROFILE=netgear_r7800 \
-PACKAGES="luci ca-bundle ca-certificates libustream-wolfssl \
+PACKAGES="luci ca-bundle ca-certificates -libustream-wolfssl libustream-mbedtls \
 https-dns-proxy luci-app-https-dns-proxy \
--wpad-mini -wpad-basic -wpad-basic-wolfssl wpad-wolfssl usbutils block-mount e2fsprogs samba4-server luci-app-samba4 \
+-wpad-mini -wpad-basic -wpad-basic-wolfssl -wpad-basic-mbedtls wpad-mbedtls usbutils block-mount e2fsprogs samba4-server luci-app-samba4 \
 aria2 luci-app-aria2 ariang curl wget kmod-fs-ext4 kmod-usb-storage kmod-usb-storage-uas \
 luci-app-statistics collectd-mod-cpu collectd-mod-interface collectd-mod-memory collectd-mod-ping collectd-mod-rrdtool collectd-mod-wireless \
 -dnsmasq dnsmasq-full luci-app-wireguard luci-proto-wireguard 6in4 \
