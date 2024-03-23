f7ab04b3bf8518ff131630257f3dac323e59bde6
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-03-23 18:35:29 +0800

    Added echo

--- latest-imagebuilder-version.sh
+++ latest-imagebuilder-version.sh
@@ -4,7 +4,9 @@
 set -e
 
 OPENWRT_VERSION=`curl -s https://api.github.com/repos/openwrt/openwrt/tags | jq -r '.[]["name"]' | grep ${3} | head -1 | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)'`
+echo "Download OpenWrt Image Builder ${OPENWRT_VERSION}"
 IMAGEBUILDER_HTTP_CODE=`curl -s -o /dev/null --head -w "%{http_code}" https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${1}/${2}/openwrt-imagebuilder-${OPENWRT_VERSION}-${1}-${2}.Linux-x86_64.tar.xz`
+echo "HTTP code: ${IMAGEBUILDER_HTTP_CODE}"
 if [[ "$IMAGEBUILDER_HTTP_CODE" != 200 ]]; then
     echo "Not found for https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${1}/${2}/openwrt-imagebuilder-${OPENWRT_VERSION}-${1}-${2}.Linux-x86_64.tar.xz"
     # Use previous version if imagebuilder not yet available
