8cc9a13755da1cf28fa0d2ec0cc2acb1001f447d
HackingGate <i@hackinggate.com>
2025-09-09 00:34:10 +0900

    Update rootfs partition size and add filesystem expansion script

--- nanopi-r6s-openwrt-imagebuilder-build.sh
+++ nanopi-r6s-openwrt-imagebuilder-build.sh
@@ -19,7 +19,7 @@ rm openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-${SUBTARGET}.Linux-x86_64.t
 cd openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-${SUBTARGET}.Linux-x86_64/
 
 # Configure rootfs partition size (1GB = 1024MB)
-sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=104/CONFIG_TARGET_ROOTFS_PARTSIZE=32768/' .config
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=104/CONFIG_TARGET_ROOTFS_PARTSIZE=256/' .config
 
 # Use https
 sed -i 's/http:/https:/g' .config repositories.conf
@@ -27,13 +27,22 @@ sed -i 's/http:/https:/g' .config repositories.conf
 # Make all kernel modules built-in
 sed -i -e "s/=m/=y/g" build_dir/target-aarch64_generic_musl/linux-${TARGET}_${SUBTARGET}/linux-*/.config
 
+# Create custom files directory structure for filesystem expansion
+mkdir -p files/etc/uci-defaults
+
+# Download the filesystem expansion script directly to uci-defaults
+curl -o files/etc/uci-defaults/99-expand-rootfs https://gist.githubusercontent.com/HackingGate/98f80db3645a3c383ea4fa179aaa4e25/raw/f2c27da2bbb53a35556100367b03f9b650766bd8/expand_rootfs.sh
+
+# Make the script executable
+chmod +x files/etc/uci-defaults/99-expand-rootfs
+
 # Run the final build configuration
 make -j$(nproc) image PROFILE=${DEVICE_NAME} \
 PACKAGES="base-files dropbear libc logd mtd opkg procd-ujail uboot-envtools uci urandom-seed urngd \
 ca-bundle ca-certificates libustream-mbedtls \
 -dnsmasq dnsmasq-full odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe 6in4 https-dns-proxy luci-proto-wireguard \
 kmod-fs-ext4 kmod-gpio-button-hotplug kmod-r8169 kmod-usb-storage kmod-usb-storage-uas kernel \
-block-mount e2fsprogs mkf2fs partx-utils resize2fs \
+block-mount e2fsprogs mkf2fs partx-utils resize2fs util-linux \
 luci luci-app-aria2 luci-app-attendedsysupgrade luci-app-cloudflared luci-app-https-dns-proxy luci-app-samba4 luci-app-statistics \
 collectd-mod-ping collectd-mod-wireless \
 aria2 ariang samba4-server tailscale \
